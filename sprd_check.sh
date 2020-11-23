83e733e5ff220791ba01736fccf3f83c7d3aefd3
Cixi Geng <cixi.geng1@unisoc.com>
2020-11-23 15:40:21 +0800

    add check attribute properties

--- sprd_check.sh
+++ sprd_check.sh
@@ -34,38 +34,42 @@ check_enabled_config()
 {
 	unset config_list
 	unset config
-	config_list=$(jq -r '.enabled_config[]' $feature_file)
-	echo_info "<<<starting check needs enabled config>>>"
-	for config in $config_list
-	do
-		zcat /proc/config.gz |grep "${config}=" >/dev/null 2>&1
-		if [ $? -ne 0 ];then
-			echo_error "$config"
-			let return_val++
-		else
-			echo_pass "$config"
-		fi
-	done
-	echo_info "<<<finished check needs enabled config>>>"
+	config_list=$(jq -r '.enabled_config[]?' $feature_file)
+	if [ -n "$config_list" ];then
+		echo_info "<<<starting check needs enabled config>>>"
+		for config in $config_list
+		do
+			zcat /proc/config.gz |grep "${config}=" >/dev/null 2>&1
+			if [ $? -ne 0 ];then
+				echo_error "$config"
+				let return_val++
+			else
+				echo_pass "$config"
+			fi
+		done
+		echo_info "<<<finished check needs enabled config>>>"
+	fi
 }
 
 check_disabled_config()
 {
 	unset config_list
 	unset config
-	config_list=$(jq -r '.disabled_config[]' $feature_file)
-	echo_info "<<<starting check needs disabled config>>>"
-	for config in $config_list
-	do
-		zcat /proc/config.gz |grep "${config}=" >/dev/null 2>&1
-		if [ $? -eq 0 ];then
-			echo_error "$config"
-			let return_val++
-		else
-			echo_pass "$config"
-		fi
-	done
-	echo_info "<<<finished check needs disabled config>>>"
+	config_list=$(jq -r '.disabled_config[]?' $feature_file)
+	if [ -n "$config_list" ];then
+		echo_info "<<<starting check needs disabled config>>>"
+		for config in $config_list
+		do
+			zcat /proc/config.gz |grep "${config}=" >/dev/null 2>&1
+			if [ $? -eq 0 ];then
+				echo_error "$config"
+				let return_val++
+			else
+				echo_pass "$config"
+			fi
+		done
+		echo_info "<<<finished check needs disabled config>>>"
+	fi
 }
 
 check_config()
@@ -73,10 +77,48 @@ check_config()
 	check_enabled_config
 	check_disabled_config
 }
-check_config
+check_properties()
+{
+	if grep $2 $1;then
+		echo_pass "$2 in $1"
+	else
+		echo_error "$2 in $1"
+		let return_val++
+	fi
+}
 
-attribute_length=$(jq -r '.attribute| length' $feature_file)
-for i in $(seq 1 ${attribute_length})
-do
-	jq -r '.attribute['$i'-1]' $feature_file
-done
+check_attribute()
+{
+	file=$(jq -r '.file' $1)
+	if ls $file >/dev/null 2>&1;then
+		echo_pass "$file"
+		properties_list=$(jq -r '.properties[]?' $1)
+		if [ -n "$properties_list" ];then
+			for prop in $properties_list
+			do
+				check_properties $file $prop
+			done
+		fi
+	else
+		echo_error "$file"
+		let return_val++
+	fi
+}
+
+check_attribute_properties()
+{
+	attribute_length=$(jq -r '.attribute| length' $feature_file)
+	if [ -n $attribute_length ];then
+		echo_info "<<<starting check attribute properties>>>"
+		for i in $(seq 1 ${attribute_length})
+		do
+			attri_file=$(mktemp)
+			jq -r '.attribute['$i'-1]?' $feature_file>$attri_file
+			check_attribute $attri_file
+		done
+		echo_info "<<<finished check attribute properties>>>"
+	fi
+}
+check_config
+check_attribute_properties
+echo $return_val
